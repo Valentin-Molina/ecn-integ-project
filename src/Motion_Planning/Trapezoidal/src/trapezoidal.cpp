30462d3578b86f2f386b47505f4ab9721626b44a
Valentin Molina <valentin@molina.pro>
2021-03-08 15:32:40 +0100

    removing trapezoidalNode.cpp

--- src/Motion_Planning/Trapezoidal/src/trapezoidal.cpp
+++ src/Motion_Planning/Trapezoidal/src/trapezoidal.cpp
@@ -1,11 +1,20 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "geometry_msgs/Pose2D.h"
+#include "../include/trapezoidalNode.h"
 
 
 void subscriberCallback(const geometry_msgs::Pose2D& msg)
 {
 	ROS_INFO("I heard: ([%f],[%f])", msg.x, msg.y);
+}
+
+TrapezoidalNode::TrapezoidalNode()
+{
+}
+
+TrapezoidalNode::~TrapezoidalNode()
+{
 }
 
 int main(int argc, char **argv)
