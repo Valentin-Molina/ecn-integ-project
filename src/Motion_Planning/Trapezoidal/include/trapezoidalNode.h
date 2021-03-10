fe3517c7e391e8d83a93f298b3f5699ce8a76be6
Valentin Molina <valentin@molina.pro>
2021-03-10 09:42:04 +0100

    ajout mgd / mgi dans trapezoidalNode

--- src/Motion_Planning/Trapezoidal/include/trapezoidalNode.h
+++ src/Motion_Planning/Trapezoidal/include/trapezoidalNode.h
@@ -6,6 +6,7 @@
 #include<ros/time.h>
 #include<vector>
 #include <geometry_msgs/Pose2D.h>
+#include <math.h>
 
 class TrapezoidalNode
 {
@@ -20,6 +21,23 @@ private:
     ros::NodeHandle nh_ ;
     // Create a publisher and name the topic.
     ros::Subscriber sub_ ;
+
+    //provisional : services will be used later
+    float l1_, l2_ ; //arm lengths
+    void mgd(const float &theta1, const float &theta2, float &x, float &y)
+    {
+        x = l1_*std::cos(theta1) + l2_*std::cos(theta1 + theta2);
+        y = l1_*std::sin(theta1) + l2_*std::sin(theta1 + theta2);
+    };
+    void mgi(const float &x, const float &y, float &theta1, float &theta2, const bool &elbowLow = true)
+    {
+        theta2 = std::acos((l1_*l1_ + l2_*l2_)/(2*l1_*l2_));
+        if(!elbowLow)
+        {
+            theta2 = - theta2 ;
+        }
+        theta1 = std::atan2(y, x) - theta2 ;
+    };
 
 };
 
