e3a03caab172350afb84799b54176e233cebc802
Valentin Molina <valentin@molina.pro>
2021-03-18 09:39:17 +0100

    update bash script for the trapezoidalNode service

--- src/Motion_Planning/Trapezoidal/TestsMotionPlanning.sh
+++ src/Motion_Planning/Trapezoidal/TestsMotionPlanning.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-#This script will publish 3 Waypoints on the topic /trapeziodal_planning/Waypoints with the last one being x=0,y=0 to trigger the compute fonction
+# This script will add 3 Waypoints via the /Waypoint_serv service with 
+# the last one being x=0,y=0 to trigger the compute fonction.
 
-rostopic pub /trapezoidal_planning/Waypoints geometry_msgs/Pose2D "x: 6.0
-y: 7.0
-theta: 0.0"
-
-
-rostopic pub /trapezoidal_planning/Waypoints geometry_msgs/Pose2D "x: 5.0
-y: -1.0
-theta: 0.0"
-
-
-rostopic pub /trapezoidal_planning/Waypoints geometry_msgs/Pose2D "x: 0.0
-y: 0.0
-theta: 0.0"
+rosservice call /Waypoint_serv "
+waypoints:
+  - {x: 1.0, y: 2.0, theta: 0.0}
+  - {x: 5.0, y: 7.0, theta: 0.0}
+  - {x: 0.0, y: 0.0, theta: 0.0}
+"
